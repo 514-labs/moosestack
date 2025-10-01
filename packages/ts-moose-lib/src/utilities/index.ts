32c08654559dbe56223d25a12f459344cb645c1e
George Leung <phisgr@gmail.com>
2025-09-30 17:52:46 -0700

    stuff

--- packages/ts-moose-lib/src/utilities/index.ts
+++ packages/ts-moose-lib/src/utilities/index.ts
@@ -1,1 +1,11 @@
 export * from "./dataParser";
+
+export type StripDateIntersection<T> =
+  T extends Date ?
+    Date extends T ?
+      Date
+    : T
+  : T extends ReadonlyArray<infer U> ? ReadonlyArray<StripDateIntersection<U>>
+  : T extends Array<infer U> ? Array<StripDateIntersection<U>>
+  : T extends object ? { [K in keyof T]: StripDateIntersection<T[K]> }
+  : T;
