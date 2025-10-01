d7a3a08f6020f3c86ca7e4cb08b933a838b12b4d
George Leung <phisgr@gmail.com>
2025-10-01 13:32:46 -0700

    avoid built-in types

--- packages/ts-moose-lib/src/utilities/index.ts
+++ packages/ts-moose-lib/src/utilities/index.ts
@@ -1,5 +1,14 @@
 export * from "./dataParser";
 
+type HasFunctionField<T> =
+  T extends object ?
+    {
+      [K in keyof T]: T[K] extends Function ? true : false;
+    }[keyof T] extends false ?
+      false
+    : true
+  : false;
+
 export type StripDateIntersection<T> =
   T extends Date ?
     Date extends T ?
@@ -10,5 +19,7 @@ export type StripDateIntersection<T> =
       ReadonlyArray<StripDateIntersection<U>>
     : Array<StripDateIntersection<U>>
   : T extends Array<infer U> ? Array<StripDateIntersection<U>>
+  : true extends HasFunctionField<T> ?
+    T // do not touch other classes
   : T extends object ? { [K in keyof T]: StripDateIntersection<T[K]> }
   : T;
