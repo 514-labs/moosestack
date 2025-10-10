7511165305b0baee9809df98fc7aac196ca13112
George Leung <phisgr@gmail.com>
2025-10-09 19:41:12 -0700

    fix test

--- packages/py-moose-lib/moose_lib/internal.py
+++ packages/py-moose-lib/moose_lib/internal.py
@@ -28,6 +28,8 @@ from moose_lib.dmv2.stream import KafkaSchemaConfig
 from pydantic.alias_generators import to_camel
 from pydantic.json_schema import JsonSchemaValue
 
+from .dmv2.olap_table import TTLConfig
+
 model_config = ConfigDict(alias_generator=AliasGenerator(
     serialization_alias=to_camel,
 ))
@@ -140,11 +142,6 @@ EngineConfigDict = Union[
     ReplicatedSummingMergeTreeConfigDict,
     S3QueueConfigDict
 ]
-
-
-class TTLConfig(BaseModel):
-    expression: Optional[str] = None
-    columns: Optional[Dict[str, str]] = None
 
 
 class TableConfig(BaseModel):
