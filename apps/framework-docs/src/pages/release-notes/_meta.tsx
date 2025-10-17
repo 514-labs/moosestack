c003298c1d91d0a9a023049710c3c85d6a13c24f
Nicolas Joseph <nicolas@fiveonefour.com>
2025-10-17 13:16:31 -0400

    change release notes

--- apps/framework-docs/src/pages/release-notes/_meta.tsx
+++ apps/framework-docs/src/pages/release-notes/_meta.tsx
@@ -7,6 +7,21 @@ const rawMeta = {
       breadcrumb: false,
     },
   },
+  "2025-07-03": {
+    title: "July 3, 2025",
+  },
+  "2025-05-30": {
+    title: "May 30, 2025",
+  },
+  "2025-05-23": {
+    title: "May 23, 2025",
+  },
+  "2025-05-19": {
+    title: "May 19, 2025",
+  },
+  "2025-05-16": {
+    title: "May 16, 2025",
+  },
   upcoming: { display: "hidden" }, // This hides it from sidebar/navigation
 };
 
