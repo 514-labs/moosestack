5d90c3d7e6372a7407e805fed5053053777f49d9
Jonathan Widjaja <smileforwidjaja@gmail.com>
2025-10-20 11:04:33 -0600

    e2e tests

---
+++ templates/typescript-migrate-test/app/ingest/models.ts
@@ -0,0 +1,28 @@
+import {
+  IngestPipeline,
+  Key,
+  OlapTable,
+  DeadLetterModel,
+  DateTime,
+} from "@514labs/moose-lib";
+
+export interface Foo {
+  primaryKey: Key<string>;
+  timestamp: number;
+  optionalText?: string;
+}
+
+export interface Bar {
+  primaryKey: Key<string>;
+  utcTimestamp: DateTime;
+  hasText: boolean;
+  textLength: number;
+}
+
+export const deadLetterTable = new OlapTable<DeadLetterModel>("FooDeadLetter", {
+  orderByFields: ["failedAt"],
+});
+
+export const FooTable = new OlapTable<Foo>("Foo");
+
+export const BarTable = new OlapTable<Bar>("Bar");
