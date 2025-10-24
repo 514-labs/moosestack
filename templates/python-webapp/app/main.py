a199cb1bcb8df95303cd7c9026cfecaa7af46d7d
Dave Seleno <958603+onelesd@users.noreply.github.com>
2025-10-23 22:33:48 -0700

    add python-webapp template

---
+++ templates/python-webapp/app/main.py
@@ -0,0 +1,5 @@
+# This file is where you will import all your data models and APIs
+# If you define any custom resource in the moosestack universe, you need to import it here
+# Example: from app.apis.bar import bar_fastapi_app
+
+from app.apis.bar import bar_fastapi_app
