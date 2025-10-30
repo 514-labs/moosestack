3cd5d3ec251d837a6f700f2537c114bd1286471d
Dave Seleno <958603+onelesd@users.noreply.github.com>
2025-10-30 12:12:00 -0600

    fix left nav

--- apps/framework-docs/src/pages/moose/apis/_meta.tsx
+++ apps/framework-docs/src/pages/moose/apis/_meta.tsx
@@ -20,6 +20,9 @@ const rawMeta = {
   "openapi-sdk": {
     title: "OpenAPI SDK",
   },
+  "bring-your-own-api-framework": {
+    title: "Bring Your Own API Framework",
+  },
   "admin-api": {
     title: "Admin APIs",
     display: "hidden",
