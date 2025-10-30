96ea0768c48da6b6fd7f7917a319c278fe1f3493
Dave Seleno <958603+onelesd@users.noreply.github.com>
2025-10-30 13:33:48 -0600

    flatten language sections

--- apps/framework-docs/src/pages/moose/app-frameworks/_meta.tsx
+++ apps/framework-docs/src/pages/moose/app-frameworks/_meta.tsx
@@ -4,11 +4,34 @@ const rawMeta = {
   index: {
     title: "Overview",
   },
-  typescript: {
-    title: "TypeScript",
+  __typescript__: {
+    type: "separator",
+    title: "TypeScript / JavaScript / Node",
   },
-  python: {
+  "typescript-native": {
+    title: "Native (Api Class)",
+  },
+  "typescript-express": {
+    title: "Express",
+  },
+  "typescript-fastify": {
+    title: "Fastify",
+  },
+  "typescript-koa": {
+    title: "Koa",
+  },
+  "typescript-raw-nodejs": {
+    title: "Raw Node.js",
+  },
+  __python__: {
+    type: "separator",
     title: "Python",
+  },
+  "python-native": {
+    title: "Native (Api Class)",
+  },
+  "python-fastapi": {
+    title: "FastAPI",
   },
 };
 
