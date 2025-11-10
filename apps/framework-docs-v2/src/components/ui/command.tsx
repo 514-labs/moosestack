eaddb9823007a762f0d0c8d48fe67e977ae5b290
Tim Delisle <tim@fiveonefour.com>
2025-11-09 18:40:32 -0800

    added default

--- apps/framework-docs-v2/src/components/ui/command.tsx
+++ apps/framework-docs-v2/src/components/ui/command.tsx
@@ -6,7 +6,7 @@ import { Command as CommandPrimitive } from "cmdk";
 import { IconSearch } from "@tabler/icons-react";
 
 import { cn } from "@/lib/utils";
-import { Dialog, DialogContent, DialogTitle } from "@/components/ui/dialog";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
 
 const Command = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
@@ -20,19 +20,15 @@ const Command = React.forwardRef<
     )}
     {...props}
   />
-)) as React.ForwardRefExoticComponent<
-  React.ComponentPropsWithoutRef<typeof CommandPrimitive> &
-    React.RefAttributes<React.ElementRef<typeof CommandPrimitive>>
->;
+));
 Command.displayName = CommandPrimitive.displayName;
 
 const CommandDialog = ({ children, ...props }: DialogProps) => {
   return (
     <Dialog {...props}>
       <DialogContent className="overflow-hidden p-0">
-        <DialogTitle className="sr-only">Search documentation</DialogTitle>
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
-          {children as React.ReactNode}
+          {children}
         </Command>
       </DialogContent>
     </Dialog>
