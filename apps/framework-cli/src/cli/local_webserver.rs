1cded9433f26be799be31c2dc1d29e60876d433a
Jonathan Widjaja <smileforwidjaja@gmail.com>
2025-11-20 16:44:19 -0700

    reality checker

--- apps/framework-cli/src/cli/local_webserver.rs
+++ apps/framework-cli/src/cli/local_webserver.rs
@@ -3572,8 +3572,9 @@ mod tests {
             unmapped_tables: vec![table.clone()],
             missing_tables: vec![],
             mismatched_tables: vec![OlapChange::Table(TableChange::Added(table.clone()))],
-            extra_sql_resources: vec![],
+            unmapped_sql_resources: vec![],
             missing_sql_resources: vec![],
+            mismatched_sql_resources: vec![],
         };
 
         let result = find_table_definition("test_table", &discrepancies);
@@ -3590,8 +3591,9 @@ mod tests {
             unmapped_tables: vec![test_table.clone()],
             missing_tables: vec![],
             mismatched_tables: vec![OlapChange::Table(TableChange::Added(test_table.clone()))],
-            extra_sql_resources: vec![],
+            unmapped_sql_resources: vec![],
             missing_sql_resources: vec![],
+            mismatched_sql_resources: vec![],
         };
 
         let mut infra_map = create_test_infra_map();
@@ -3624,8 +3626,9 @@ mod tests {
             unmapped_tables: vec![test_table.clone()],
             missing_tables: vec![],
             mismatched_tables: vec![OlapChange::Table(TableChange::Added(test_table.clone()))],
-            extra_sql_resources: vec![],
+            unmapped_sql_resources: vec![],
             missing_sql_resources: vec![],
+            mismatched_sql_resources: vec![],
         };
 
         let mut infra_map = create_test_infra_map();
