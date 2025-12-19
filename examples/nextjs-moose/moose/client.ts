ad5271b18498fae2231e0b9b2f16dd91790c96e3
Olivia Kane <olivia@fiveonefour.com>
2025-12-19 15:21:20 -0500

    make client and query examples the same

--- examples/nextjs-moose/moose/client.ts
+++ examples/nextjs-moose/moose/client.ts
@@ -1,26 +1,25 @@
-import { getMooseClients } from "@514labs/moose-lib";
+import { getMooseClients, Sql } from "@514labs/moose-lib";
 
-const globalForMoose = globalThis as unknown as {
-  mooseClient: Awaited<ReturnType<typeof getMooseClients>> | undefined;
-};
+let clientsPromise:
+  | Promise<Awaited<ReturnType<typeof getMooseClients>>>
+  | undefined;
 
-export async function getMoose() {
-  if (globalForMoose.mooseClient) {
-    return globalForMoose.mooseClient;
-  }
-
-  const client = await getMooseClients({
-    database: process.env.MOOSE_CLICKHOUSE_CONFIG__DB_NAME!,
-    host: process.env.MOOSE_CLICKHOUSE_CONFIG__HOST!,
-    port: process.env.MOOSE_CLICKHOUSE_CONFIG__PORT!,
-    username: process.env.MOOSE_CLICKHOUSE_CONFIG__USER!,
-    password: process.env.MOOSE_CLICKHOUSE_CONFIG__PASSWORD!,
-    useSSL: process.env.MOOSE_CLICKHOUSE_CONFIG__USE_SSL === "true",
+async function getMoose() {
+  clientsPromise ??= getMooseClients({
+    host: process.env.MOOSE_CLICKHOUSE_CONFIG__HOST ?? "localhost",
+    port: process.env.MOOSE_CLICKHOUSE_CONFIG__PORT ?? "18123",
+    username: process.env.MOOSE_CLICKHOUSE_CONFIG__USER ?? "panda",
+    password: process.env.MOOSE_CLICKHOUSE_CONFIG__PASSWORD ?? "pandapass",
+    database: process.env.MOOSE_CLICKHOUSE_CONFIG__DB_NAME ?? "local",
+    useSSL:
+      (process.env.MOOSE_CLICKHOUSE_CONFIG__USE_SSL ?? "false") === "true",
   });
 
-  if (process.env.NODE_ENV !== "production") {
-    globalForMoose.mooseClient = client;
-  }
+  return await clientsPromise;
+}
 
-  return client;
+export async function executeQuery<T>(query: Sql): Promise<T[]> {
+  const { client } = await getMoose();
+  const result = await client.query.execute(query);
+  return result.json();
 }
