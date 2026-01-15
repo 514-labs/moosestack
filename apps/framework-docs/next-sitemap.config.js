4f5aaf35235f40ca23e5dce026c98c1d0434ebb0
Nicolas Joseph <nicolas@fiveonefour.com>
2026-01-14 23:20:46 -0500

    remove v1 version of the docs

--- apps/framework-docs/next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.SITE_URL || "https://docs.fiveonefour.com/",
-  generateRobotsTxt: false, // (optional)
-  exclude: ["*/_meta"],
-  // ...other options
-};
