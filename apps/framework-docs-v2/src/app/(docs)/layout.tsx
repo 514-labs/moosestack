a71d3c8514ed5ad012457045de50b1384b00e9dc
Tim Delisle <tim@fiveonefour.com>
2026-01-22 09:55:44 -0800

    add analytics back in

--- apps/framework-docs-v2/src/app/(docs)/layout.tsx
+++ apps/framework-docs-v2/src/app/(docs)/layout.tsx
@@ -2,6 +2,7 @@ import type { ReactNode } from "react";
 import { Suspense } from "react";
 import { SideNavServer } from "@/components/navigation/side-nav-server";
 import { SidebarInset } from "@/components/ui/sidebar";
+import { AnalyticsProvider } from "@/components/analytics-provider";
 
 interface DocLayoutProps {
   children: ReactNode;
@@ -9,18 +10,20 @@ interface DocLayoutProps {
 
 export default function DocLayout({ children }: DocLayoutProps) {
   return (
-    <div className="flex flex-1">
-      <Suspense fallback={<div className="w-64" />}>
-        <SideNavServer />
-      </Suspense>
-      <SidebarInset>
-        <div className="container flex-1 pt-6 pb-12 lg:pt-8">
-          {/* Reserve space for the right TOC on xl+ screens */}
-          <main className="relative flex flex-col gap-10 xl:grid xl:grid-cols-[minmax(0,1fr)_240px] xl:gap-12">
-            {children}
-          </main>
-        </div>
-      </SidebarInset>
-    </div>
+    <AnalyticsProvider>
+      <div className="flex flex-1">
+        <Suspense fallback={<div className="w-64" />}>
+          <SideNavServer />
+        </Suspense>
+        <SidebarInset>
+          <div className="container flex-1 pt-6 pb-12 lg:pt-8">
+            {/* Reserve space for the right TOC on xl+ screens */}
+            <main className="relative flex flex-col gap-10 xl:grid xl:grid-cols-[minmax(0,1fr)_240px] xl:gap-12">
+              {children}
+            </main>
+          </div>
+        </SidebarInset>
+      </div>
+    </AnalyticsProvider>
   );
 }
