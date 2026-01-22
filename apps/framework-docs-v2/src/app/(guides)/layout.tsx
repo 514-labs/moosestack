0dd1e06c7da13d7ac6999a998da4b4d5b5c429a7
Nicolas Joseph <nicolas@fiveonefour.com>
2026-01-22 13:47:19 -0500

    update layoout

--- apps/framework-docs-v2/src/app/(guides)/layout.tsx
+++ apps/framework-docs-v2/src/app/(guides)/layout.tsx
@@ -8,17 +8,10 @@ interface GuidesLayoutProps {
 export default function GuidesLayout({ children }: GuidesLayoutProps) {
   return (
     <AnalyticsProvider>
-      <div className="flex flex-1">
-        {/* Invisible spacer to match sidebar width - compresses first before content */}
-        <div className="hidden md:block flex-none lg:flex-[0_1_16rem] min-w-0" />
-        <div className="flex-[1_0_48rem] min-w-[48rem]">
-          <div className="container mx-auto pt-6 pb-12 lg:pt-8">
-            {/* Match the grid layout of guide pages (reserves space for TOC on right) */}
-            <main className="relative flex flex-col gap-10 xl:grid xl:grid-cols-[minmax(0,1fr)_240px] xl:gap-12">
-              {children}
-              {/* Empty TOC space to match guide page layout */}
-              <div className="hidden xl:block" />
-            </main>
+      <div className="flex flex-1 justify-center">
+        <div className="w-full max-w-4xl px-8">
+          <div className="pt-6 pb-12 lg:pt-8">
+            <main className="relative">{children}</main>
           </div>
         </div>
       </div>
