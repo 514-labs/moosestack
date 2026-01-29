6c4b0a433a972d4ece29415f1a06d21992b207ee
Alex <alex.lakas@gmail.com>
2026-01-29 12:25:48 -0800

    Expand max-width to 6xl on large breakpoints

--- apps/framework-docs-v2/src/app/(guides)/layout.tsx
+++ apps/framework-docs-v2/src/app/(guides)/layout.tsx
@@ -9,7 +9,7 @@ export default function GuidesLayout({ children }: GuidesLayoutProps) {
   return (
     <AnalyticsProvider>
       <div className="flex flex-1 justify-center min-w-0">
-        <div className="w-full max-w-4xl px-4 sm:px-6 lg:px-8 min-w-0">
+        <div className="w-full max-w-5xl lg:max-w-6xl px-4 sm:px-6 lg:px-8 min-w-0">
           <div className="pt-6 pb-12 lg:pt-8">
             <main className="relative min-w-0">{children}</main>
           </div>
