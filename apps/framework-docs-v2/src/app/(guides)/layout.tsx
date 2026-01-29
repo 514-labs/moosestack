3877c5d4506694510bbcfb67cad9918a2bc4a048
Alex <alex.lakas@gmail.com>
2026-01-29 13:54:57 -0800

    Match guides page max-width to docs pages

--- apps/framework-docs-v2/src/app/(guides)/layout.tsx
+++ apps/framework-docs-v2/src/app/(guides)/layout.tsx
@@ -12,7 +12,7 @@ export default function GuidesLayout({ children }: GuidesLayoutProps) {
       <div className="flex flex-1 min-w-0 w-full overflow-hidden">
         <SidebarInset>
           <div className="w-full min-w-0 flex-1 pt-6 pb-12 lg:pt-8 px-4 sm:px-6 lg:px-8 overflow-x-hidden">
-            <main className="relative flex flex-col gap-10 min-w-0 w-full max-w-3xl lg:max-w-4xl mx-auto">
+            <main className="relative flex flex-col gap-10 min-w-0 w-full max-w-5xl lg:max-w-6xl mx-auto">
               {children}
             </main>
           </div>
