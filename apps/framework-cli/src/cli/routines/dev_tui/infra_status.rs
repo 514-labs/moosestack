b3001a53d5550c0ff68d6aebe045eb5b56d3b866
Nicolas Joseph <nicolas@fiveonefour.com>
2026-02-12 15:47:10 -0500

    feat(cli): add infrastructure boot with TUI status channels
    Add infrastructure startup integration for TUI mode:
    - dev.rs: InfraBootError + run_infrastructure_with_updates function
      that sends real-time status updates during infrastructure boot
    - watcher.rs: Add resource_update_tx parameter for TUI resource updates
    - routines/mod.rs: Update watcher.start() call to pass None
    
    These functions are marked with #[allow(dead_code)] temporarily
    as they'll be called from TUI entry point in PR5.

--- apps/framework-cli/src/cli/routines/dev_tui/infra_status.rs
+++ apps/framework-cli/src/cli/routines/dev_tui/infra_status.rs
@@ -3,9 +3,6 @@
 //! This module defines the status types used to communicate infrastructure boot
 //! progress from the background infrastructure task to the TUI for real-time display.
 
-// TODO(PR4): Remove this allow once dev.rs uses these types
-#![allow(dead_code)]
-
 use tokio::sync::mpsc;
 
 /// Boot phase during infrastructure startup
@@ -206,9 +203,11 @@ pub enum InfraStatusUpdate {
 pub type InfraStatusSender = mpsc::UnboundedSender<InfraStatusUpdate>;
 
 /// Receiver for infrastructure status updates
+#[allow(dead_code)] // TODO(PR5): Remove once entry point uses this
 pub type InfraStatusReceiver = mpsc::UnboundedReceiver<InfraStatusUpdate>;
 
 /// Creates a new channel for infrastructure status updates
+#[allow(dead_code)] // TODO(PR5): Remove once entry point uses this
 pub fn infra_status_channel() -> (InfraStatusSender, InfraStatusReceiver) {
     mpsc::unbounded_channel()
 }
